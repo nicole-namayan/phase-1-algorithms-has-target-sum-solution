684451224bb384f2ee1b8d2c3b5de482c2c51527
nicole <nicolenamayan133@gmail.com>
2023-03-31 11:21:17 +0300

    completed

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+
+  for(const number of array) {
+    // n step
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+
+  seenNumbers[number] = true;
+  }
+  return false;
 }
 
 /* 
@@ -25,10 +36,23 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8],3));
+
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+
 }
 
 module.exports = hasTargetSum;
